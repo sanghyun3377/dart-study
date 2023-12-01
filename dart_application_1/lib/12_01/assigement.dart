168e39d7dff5be889184e8b8e1fe64eac5595c4b
sanghyun3377 <iccompany3377@gmail.com>
2023-12-01 15:05:12 +0900

    연습문제 2-2

--- dart_application_1/lib/12_01/assigement.dart
+++ dart_application_1/lib/12_01/assigement.dart
@@ -7,14 +7,16 @@ class Cleric {
   static final int maxMP = 10;
 
   String name;
-  int hp = 50;
-  int mp = 10;
+  int hp;
+  int mp;
 
-  Cleric({
-    required this.name,
-    required this.hp,
-    required this.mp,
-  });
+//이 클래스는 Cleric(“아서스", hp: 40, mp: 5) 와 같이, 이름, HP, MP 를 지정하여 인스턴스화 할 수 있다
+//이 클래스는 Cleric(“아서스", hp: 35) 와 같이, 이름과 HP만으로 지정하여 인스턴스화 할 수 있다. 이 때, MP는 최대 MP와 같은 값이 초기화 된다
+//이 클래스는 Cleric(“아서스") 와 같이 이름만을 지정하여 인스턴스화 할 수 있다. 이 때, HP 와 MP 는 최대 HP와 최대 MP로 초기화 된다
+//이 클래스는 Cleric() 과 같이 이름을 지정하지 않는 경우에는 인스턴스화 할 수 없다고 한다. (이름이 없는 성직자는 존재 할 수 없음)
+//생성자는 가능한 한 중복되는 코드가 없도록 작성한다
+
+  Cleric(this.name, {this.hp = 50, this.mp = 10});
 
   //마나 5를 소비해서 체력을 채우기
   selfAid() {
@@ -43,7 +45,7 @@ class Cleric {
 }
 
 void main() {
-  Cleric cleric1 = Cleric(name: 'name', hp: 32, mp: 8);
-  cleric1.selfAid();
-  cleric1.pray(1);
+  Cleric cleric1 = Cleric("아서스", hp: 40, mp: 5);
+  Cleric cleric2 = Cleric("아서스", hp: 35, mp: Cleric.maxMP);
+  Cleric cleric3 = Cleric("아서스", hp: Cleric.maxHP, mp: Cleric.maxMP);
 }
