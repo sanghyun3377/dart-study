34d85b26a7893c537fce54e3c9151092de03dcb5
sanghyun3377 <iccompany3377@gmail.com>
2023-12-04 15:48:55 +0900

    연습문제 10-2

--- dart_application_1/lib/12_01/test.dart
+++ dart_application_1/lib/12_01/test.dart
@@ -1,13 +1,10 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class Human {
-  String name;
+  String name = '상현';
   int age;
-  Human(
-    this.name,
-    this.age,
-  );
+  Human(this.age, {this.name = '민수'});
 }
 
 void main() {
-  Human('상현', 100);
+  Human(100);
 }
--- dart_application_1/lib/12_04/assigement.dart
+++ dart_application_1/lib/12_04/assigement.dart
@@ -1,20 +1,48 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+//10-1 다음 2개의 클래스 “Wizard (마법사)”, “Wand (지팡이)” 를 작성하시오. 마법사는 지팡이를 들 수 있습니다.
+
+// 마법사나 지팡이의 이름은 null 일 수 없고, 반드시 3문자 이상이어야 한다
+// 지팡이의 마력은 0.5 이상 100.0 이하여야 한다.
+// 마법사의 지팡이는 null 일 수 없다.
+// 마법사의 MP는 0 이상이어야 한다.
+// HP가 음수가 되는 상황에서는 대신 0을 설정 되도록 한다. (에러 아님)
 class Wand {
   String name;
   double power;
+
   Wand({
     required this.name,
     required this.power,
-  });
+  }) {
+    if (name.length < 3) {
+      throw Exception("Wand의 이름이 3글자 미만입니다");
+    }
+    if (power < 0.5 || power > 100) {
+      throw Exception("지팡이의 마력 범위에러");
+    }
+  }
 }
 
 class Wizard {
   String name;
   int hp;
-  Wand? wand;
+  int mp;
+  Wand wand;
   Wizard({
     required this.name,
     required this.hp,
-    this.wand,
-  });
+    required this.mp,
+    required this.wand,
+  }) {
+    if (name.length < 3) {
+      throw Exception("Wizard의 이름이 3글자 미만입니다");
+    }
+    if (mp < 0) {
+      throw Exception("마나는 0이상이어야 합니다");
+    }
+    if (hp < 0) {
+      hp = 0;
+    }
+  }
 }
+
+void main() {}
