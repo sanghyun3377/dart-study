a1d38159f3779ae2d8f91896262eab31dbc0aad9
sanghyun3377 <iccompany3377@gmail.com>
2023-12-04 17:02:23 +0900

    연습문제 2-2

--- dart_application_1/lib/12_04/assigement2.dart
+++ dart_application_1/lib/12_04/assigement2.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 // 대한민국의 도시 이름 모음 (순서 상관 없음)
 // 10명 학생의 시험 점수
 // 대한민국의 도시별 인구수 (순서 상관 없음)
@@ -8,8 +9,30 @@ Map<String, int> testScores = {
   '10번 학생': 20,
 };
 
-Map<String, int> Population = {
+Map<String, int> population = {
   '서울': 140,
   '대구': 30,
   '부산': 70,
 };
+
+// 이름을 가지는 Person 클래스를 작성하시오. Person 은 반드시 이름을 포함해야 합니다.
+// 이름이 ‘홍길동', ‘한석봉' 인 Person 인스턴스를 생성하고, List에 담습니다.
+// List에 담긴 모든 Person 인스턴스의 이름을 표시하시오.
+
+class Person {
+  String name;
+  Person({
+    required this.name,
+  });
+}
+
+void main() {
+  List personNameList = [];
+  Person person1 = Person(name: '홍길동');
+  Person person2 = Person(name: '한석봉');
+
+  personNameList.add(person1.name);
+  personNameList.add(person2.name);
+
+  print(personNameList);
+}
