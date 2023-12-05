7a88c9334fd5a5b04f7ceb07f6c32f9000bb247e
sanghyun3377 <iccompany3377@gmail.com>
2023-12-05 15:18:44 +0900

    연습문제 12-2

--- dart_application_1/lib/12_05/assignment2.dart
+++ dart_application_1/lib/12_05/assignment2.dart
@@ -3,14 +3,20 @@
 // 어떤 회사에서 자산관리 프로그램을 만들려고 한다. 현시점에서 컴퓨터, 책 을 표현하는, 다음과 같은 두개의 클래스가 있다
 // 이후, 컴퓨터와 책 이외에도 여러가지 자산을 관리하고 싶은 경우에 유용한 유형자산(TangibleAsset) 이라는 이름의 추상클래스를 작성 하시오. 또, Computer 나 Book 은 그 부모 클래스를 활용한 형태로 수정 하시오.
 
-//유형자산 추상클래스
-abstract class TangibleAsset {
+//자산 추상클래스
+abstract class Asset {
   String name;
+
+  Asset({required this.name});
+}
+
+//유형자산 추상클래스
+abstract class TangibleAsset extends Asset {
   int price;
   String color;
 
   TangibleAsset({
-    required this.name,
+    required super.name,
     required this.price,
     required this.color,
   });
