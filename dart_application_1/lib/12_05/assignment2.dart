f533a23703161d4f6465c74d2f21fa10a637fab5
sanghyun3377 <iccompany3377@gmail.com>
2023-12-05 17:51:42 +0900

    연습문제 12-3

--- dart_application_1/lib/12_05/assignment2.dart
+++ dart_application_1/lib/12_05/assignment2.dart
@@ -3,6 +3,23 @@
 // 어떤 회사에서 자산관리 프로그램을 만들려고 한다. 현시점에서 컴퓨터, 책 을 표현하는, 다음과 같은 두개의 클래스가 있다
 // 이후, 컴퓨터와 책 이외에도 여러가지 자산을 관리하고 싶은 경우에 유용한 유형자산(TangibleAsset) 이라는 이름의 추상클래스를 작성 하시오. 또, Computer 나 Book 은 그 부모 클래스를 활용한 형태로 수정 하시오.
 
+// 연습문제 12-2
+// 다음의 상속도의 가, 나, 다 부분의 클래스명을 생각 해 보시오.
+// 가 : Asset
+// 나 : IntangibleAsset
+// 다 : Patent
+
+// 연습문제 12-3
+// 자산인지 아닌지 따지지 말고, 형태가 있는 것 (Thing) 이면, 무게가 있다
+// 그래서, double 형으로 무게(weight)를 얻을 수 있도록 getter/setter를 가지는 인터페이스 Thing 을 만드시오
+
+// 인터페이스
+abstract interface class Thing {
+  double get weight;
+
+  set setWeight(double weight);
+}
+
 //자산 추상클래스
 abstract class Asset {
   String name;
