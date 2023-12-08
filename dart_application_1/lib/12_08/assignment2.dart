f3ba214c5544c611f1ec8889ea08ca52e36c6f94
sanghyun3377 <iccompany3377@gmail.com>
2023-12-08 19:00:47 +0900

    문자열 조작 실습 2

--- dart_application_1/lib/12_08/assignment2.dart
+++ dart_application_1/lib/12_08/assignment2.dart
@@ -2,11 +2,12 @@
 // 영어에서의 모음은 a, e, i, o, u 다섯가지이다
 
 class Word {
-  String word = 'eaff';
+  String word = 'eafftest';
 
+  //실습 1번
   bool isVowel(int i) {
-    String data = word.substring(i, i + 1).toLowerCase();
-    return ('aeiou').contains(data);
+    String vowel = word.substring(i, i + 1).toLowerCase();
+    return ('aeiou').contains(vowel);
   }
 
   // bool isVowel(int i) {
@@ -17,9 +18,18 @@ class Word {
   //       data == 'o' ||
   //       data == 'u';
   // }
+
+//실습 2번
+  bool isConsonant(int i) {
+    var consonant = word.substring(i, i + 1).toLowerCase();
+    return ('bcdfghjklmnpqrstvwxyz').contains(consonant);
+  }
 }
 
 void main() {
   var res = Word().isVowel(0);
   print(res);
+
+  var res2 = Word().isConsonant(5);
+  print(res2);
 }
