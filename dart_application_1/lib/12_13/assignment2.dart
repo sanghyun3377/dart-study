65122aa2d60c93e05a6d616e32b77ff81608d880
sanghyun3377 <iccompany3377@gmail.com>
2023-12-13 18:33:21 +0900

    비동기 프로그래밍 연습 2.API 호출 및 데이터처리

--- dart_application_1/lib/12_13/assignment2.dart
+++ dart_application_1/lib/12_13/assignment2.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
 Future<String> getMovieInfo() async {
@@ -8,4 +9,36 @@ Future<String> getMovieInfo() async {
     'year': 1977,
   };
   return jsonEncode((mockData));
+}
+
+class MockData {
+  String title;
+  String director;
+  int year;
+  MockData({
+    required this.title,
+    required this.director,
+    required this.year,
+  });
+
+  factory MockData.fromMap(Map<String, dynamic> map) {
+    return MockData(
+      title: map['title'] as String,
+      director: map['director'] as String,
+      year: map['year'] as int,
+    );
+  }
+
+  factory MockData.fromJson(String source) =>
+      MockData.fromMap(json.decode(source) as Map<String, dynamic>);
+}
+
+void main() {
+  Future directorName() async {
+    var res = await getMovieInfo();
+    var data = MockData.fromJson(res);
+    print(data.director);
+  }
+
+  directorName();
 }
