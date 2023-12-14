2393ae24769492b177c198365b2a8b6c3c3de227
sanghyun3377 <iccompany3377@gmail.com>
2023-12-14 22:11:49 +0900

    네트워크 통신 연습문제 6: 에러처리

--- dart_application_1/lib/12_14/assignment3_4_6.dart
+++ dart_application_1/lib/12_14/assignment3_4_6.dart
@@ -3,8 +3,16 @@ import 'package:http/http.dart' as http;
 import 'dart:io';
 
 Future<Uint8List> downloadImage(String url) async {
-  var response = await http.get(Uri.parse(url));
-  return response.bodyBytes;
+  Uint8List data;
+  try {
+    var response = await http.get(Uri.parse(url));
+    data = response.bodyBytes;
+  } catch (e) {
+    var file = File('lib/12_14/icon.ico');
+    print('잘못된 요청입니다.');
+    data = await file.readAsBytes();
+  }
+  return data;
 }
 
 Future<File> saveFile(Uint8List bytes, String fileName) async {
@@ -25,9 +33,5 @@ printDownloadInfo(Future<Uint8List> function) async {
 }
 
 void main() async {
-  await printDownloadInfo(downloadImage('https://alimipro.com/favicon.ico'));
-  await downloadImage(
-      'https://images.unsplash.com/photo-1702325250074-28bc0f34646e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
-  await downloadImage(
-      'https://images.unsplash.com/photo-1695653422536-d1e9d449ce7b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
+  printDownloadInfo(downloadImage('https://alcon.ico'));
 }
