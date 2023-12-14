bad37fb079b8e8eb941f7cec26e6a204dad4a030
sanghyun3377 <iccompany3377@gmail.com>
2023-12-14 18:32:40 +0900

    네트워크 통신 연습문제 4. 다운로드 시간, 용량 표시

--- dart_application_1/lib/12_14/assignment3.dart
+++ dart_application_1/lib/12_14/assignment3.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 import 'dart:io';
+import 'dart:developer';
 
 Future<Uint8List> downloadImage(String url) async {
   var response = await http.get(Uri.parse(url));
@@ -13,6 +14,13 @@ Future<File> saveFile(Uint8List bytes, String fileName) async {
 }
 
 void main() async {
+  print('다운로드 시작');
+  var stopWatch = Stopwatch()..start();
   var alimiproImage = await downloadImage('https://alimipro.com/favicon.ico');
+  stopWatch.stop();
+  print('다운로드 끝');
+  print('=========');
+  print('소요시간 : ${stopWatch.elapsed}');
   saveFile(alimiproImage, 'icon.ico');
+  print('용량 : ${alimiproImage.lengthInBytes}bytes');
 }
